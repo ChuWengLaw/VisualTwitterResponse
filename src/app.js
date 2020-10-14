efa15b8fd93eaa6f42b88ec7bf842c02f6703615
Callum <62454246+Colonel-Kentucky@users.noreply.github.com>
2020-10-14 13:11:59 +1000

    Twit

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
 /*
 Callum
+Consumer Keys
 API KEY:  8DEciTD5bVy4HQdSIXRevYFZI 
 secret key: lW1I6Sc3xMxDv8QNqz3Pbefe5ANbVTa91ldonJFxpii95IVHAv
+
+Auth
 bearer token: AAAAAAAAAAAAAAAAAAAAANpSIgEAAAAA9nGL%2FC2daCSOKX6TC4jMVYMDwvc%3D3a4Ak9TiKvRn98ceC2yII7w0zTqp3ollbO3xAmGRODDTR2b4Sn
+Access token:1072388204534292480-4f6rYbuBeZ9r00Ry6xkaa5bW2Qj5eX
+Access token secret:0qdK02JjKNjerysctpWgVFUU3Dvn1jJqwtmsIr1uiJICS
 */
 var createError = require('http-errors');
 var express = require('express');
@@ -12,6 +17,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
+
 
 var app = express();
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -6,8 +6,31 @@ const router = express.Router();
 
 router.use(logger('tiny'));
 
+var Twit = require('twit')
+ 
+var T = new Twit({
+  consumer_key:         '8DEciTD5bVy4HQdSIXRevYFZI',
+  consumer_secret:      'lW1I6Sc3xMxDv8QNqz3Pbefe5ANbVTa91ldonJFxpii95IVHAv',
+  access_token:         '1072388204534292480-4f6rYbuBeZ9r00Ry6xkaa5bW2Qj5eX',
+  access_token_secret:  '0qdK02JjKNjerysctpWgVFUU3Dvn1jJqwtmsIr1uiJICS',
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+  strictSSL:            true,     // optional - requires SSL certificates to be valid.
+})
+var rsp;
 /* Render home page. */
 router.get('/', (req, res) => {
+  T.get('trends/place', { id: '1100661' }, function(err, data, response) {
+    for (var i = 0; i<data[0].trends.length;i++)
+    {
+      if (data[0].trends[i].tweet_volume != null)
+      {
+        console.log(data[0].trends[i]);
+      }
+        
+    }
+    
+  })
+  
   res.render('index');
 });
 
