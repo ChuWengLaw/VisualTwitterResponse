f41c8ca7415935e6b5f65eddf9643656ccf6874e
Eddy <carlos.law@outlook.com>
2020-10-15 11:03:31 +0800

    Fully functional show 3 trending topics' tweets by lcw

--- src/public/javascripts/twitter.js
+++ src/public/javascripts/twitter.js
@@ -5,10 +5,31 @@ function showTweet(e) {
         $.ajax({
             url: "/search?location=" + input, success: function (result) {
                 let postDiv = document.getElementsByClassName('post1');
-                    postDiv[0].innerHTML = JSON.stringify(result);
-                }
-            })
-    } catch(error) {
+                postDiv[0].innerHTML = JSON.stringify(result[0]);
+                let postDiv2 = document.getElementsByClassName('post2');
+                postDiv2[0].innerHTML = JSON.stringify(result[1]);
+                let postDiv3 = document.getElementsByClassName('post3');
+                postDiv3[0].innerHTML = JSON.stringify(result[2]);
+            }
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+/* Retrieve chart info */
+function showChart(e) {
+    try {
+        let input = document.getElementsByClassName("form-control")[0].value;
+        $.ajax({
+            url: "/chart?location=" + input, success: function (result) {
+                let postDiv = document.getElementById('chart');
+                postDiv.src = result;
+                //var ctx = postDiv.getContext("2d");
+                //ctx.drawImage(result, 10, 10);
+            }
+        })
+    } catch (error) {
         console.log(error);
     }
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,10 +4,7 @@ const https = require('https');
 const logger = require('morgan');
 const router = express.Router();
 
-//write to file delete if not needed
-var fs = require('fs');
-
-
+var Promise = require('es6-promise').Promise;
 
 router.use(logger('tiny'));
 
@@ -27,7 +24,53 @@ var rsp;
 var Location = 'Australia';
 /* Render home page. */
 router.get('/', (req, res) => {
-  var CityName_URL = `http://api.openweathermap.org/data/2.5/weather?q=${Location}&appid=${OWMKey}`
+  res.render('index');
+});
+
+
+/* Search trending twitter posts */
+router.get('/search', (req, res) => {
+  var CityName_URL = `http://api.openweathermap.org/data/2.5/weather?q=${req.query.location}&appid=${OWMKey}`
+  axios.get(CityName_URL) //used to return longandlat
+    .then((response) => {
+      const rsp = response.data;
+      //used to return woeid of a place
+      T.get('trends/closest', { lat: rsp.coord.lat, long: rsp.coord.lon }, function (err, data, response2) {
+        var Location_WoeID = data[0].woeid;
+        //used to return trending name in the place
+        T.get('trends/place', { id: Location_WoeID }, function (err, data2, response3) {
+          //search for the relevant tweets
+          Promise.all(
+            data2[0].trends.slice(0, 3).map(trend => {
+              return new Promise((resolve, reject) => {
+                T.get('search/tweets', { q: JSON.stringify(trend.name), count: 1 }, function (err, data3, response) {
+                  try {
+                    resolve(data3.statuses[0].text);
+                  } catch (err) {
+                    console.log("Rate limit reached!!");
+                    reject("Rate limit reached!!");
+                  }
+                })
+
+              })
+            })
+          ).then(result => {
+            res.send(result);
+          }).catch(error => {
+            console.log(error);
+          })
+        })
+      })
+    })
+    .catch(error => {
+      console.log(error);
+      res.render('error', { error });
+    });
+});
+
+/* Show charts */
+router.get('/chart', (req, res) => {
+  var CityName_URL = `http://api.openweathermap.org/data/2.5/weather?q=${req.query.location}&appid=${OWMKey}`;
   axios.get(CityName_URL)
     .then((response) => {
       const rsp = response.data;
@@ -36,12 +79,9 @@ router.get('/', (req, res) => {
         var Location_WoeID = data[0].woeid
         T.get('trends/place', { id: Location_WoeID }, function (err, data, response) {
 
-
           var mystring = JSON.stringify(data);
           mystring = mystring.split('#').join('');
           data = JSON.parse(mystring);
-
-
 
           ChartURL = `https://quickchart.io/chart?c={type:'bar',data:{labels:[`
           for (var i = 0; i < data[0].trends.length; i++) {
@@ -51,52 +91,18 @@ router.get('/', (req, res) => {
           }
           ChartURL = ChartURL.slice(0, -1)
 
-
           ChartURL = ChartURL + `],datasets:[{label:'Users',data:[`
           for (var i = 0; i < data[0].trends.length; i++) {
             if (data[0].trends[i].tweet_volume != null) {
               ChartURL = ChartURL + data[0].trends[i].tweet_volume + `,`;
             }
           }
-          ChartURL = ChartURL.slice(0, -1)
+          ChartURL = ChartURL.slice(0, -1);
           ChartURL = ChartURL + `]}]}}`;
-
           console.log(ChartURL);
-          res.render('index');
-                  
-        })
-      })      
-    });
-});
-
-
-/* Search trending twitter posts */
-router.get('/search', (req, res) => {
-  var CityName_URL = `http://api.openweathermap.org/data/2.5/weather?q=${req.query.location}&appid=${OWMKey}`
-  axios.get(CityName_URL) //used to return longandlat
-    .then(async (response) => {
-      var arr = [];
-      const rsp = response.data;
-      //used to return woeid of a place
-      T.get('trends/closest', { lat: rsp.coord.lat, long: rsp.coord.lon }, async function (err, data, response2) {
-        var Location_WoeID = data[0].woeid;
-        //used to return trending name in the place
-        T.get('trends/place', { id: Location_WoeID }, async function (err, data2, response3) {
-          //search for the relevant tweets
-          for (var i = 0; i < 3; i++) {
-            T.get('search/tweets', { q: JSON.stringify(data2[0].trends[i].name), count: 1 }, function (err, data3, response) {
-              arr.push(data3.statuses[0].text);
-              //arr.push(i);
-              //console.log(data3.statuses[0].text);
-            })
-          }
+          res.send(ChartURL);
         })
       })
-      res.send(arr);
-    })
-    .catch(error => {
-      console.log(error);
-      res.render('error', { error });
     });
 });
 
