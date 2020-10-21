93672d5efe63195fe56cc8fe1eae6b3c657f7902
Callum <62454246+Colonel-Kentucky@users.noreply.github.com>
2020-10-21 12:54:26 +1000

    Revert "Merge branch 'lcw' into Dev"
    This reverts commit d5b869c3b7bd59a3e70ff3a1b3d04c8b039d31b9, reversing
    changes made to 964590ea3c2a32ff98c7268150faee56cd4aa5c5.

--- src/routes/index.js
+++ src/routes/index.js
@@ -83,10 +83,7 @@ router.get('/', (req, res) => {
 /* Search trending twitter posts */
 router.get('/search', (req, res) => {
   var CityName_URL = `http://api.openweathermap.org/data/2.5/weather?q=${req.query.location}&appid=${OWMKey}`
-  // Remove space from country name for redis
-  var temp = `${req.query.location}`;
-  var country = temp.replace(" ", "");
-  const redisKey = `twitter:${country}`;
+  const redisKey = `twitter:${req.query.location}`;
   const s3Key = `twitter:${req.query.location}`;
 
   // Try the cache   
@@ -153,7 +150,6 @@ router.get('/search', (req, res) => {
                       return new Promise((resolve, reject) => {
                         T.get('search/tweets', { q: JSON.stringify(trend.name), count: 1 }, function (err, data3, response) {
                           try {
-                            console.log(data3);
                             //do this first then send to ajax
                             resolve(data3.statuses[0].text);
                           } catch (err) {
@@ -186,7 +182,6 @@ router.get('/search', (req, res) => {
                     return res.send(JSONResult);
                   }).catch(error => {
                     console.log(error);
-                    res.render('error', { error });
                   })
                 })
               })
