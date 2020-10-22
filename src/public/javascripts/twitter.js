4ec87954b42f2e577501e4fe4b5123c463db85a3
Callum <62454246+Colonel-Kentucky@users.noreply.github.com>
2020-10-22 14:05:35 +1000

    Update twitter.js

--- src/public/javascripts/twitter.js
+++ src/public/javascripts/twitter.js
@@ -6,14 +6,15 @@ function showTweet(e) {
         $.ajax({
             //Redirect to router funciton in index.js
             url: "/search?location=" + input, success: function (result) {
-                var JSONResult = JSON.parse(result);
-
+                console.log(result);
                 if (result === "") {
                     console.log("catched in twitter.js");
-                    console.log(JSONResult);
                     console.log(result);
                     $("#myModal").modal();
                 }
+                var JSONResult = JSON.parse(result);
+
+
                 
                 //Render twitter topics in html
                 let topic1 = document.getElementsByClassName('topic1');
